638174a85a25ccf3f20ff7c4c7be0b1415c75407
soly179 <solyelsa@gmail.com>
2021-03-21 15:38:39 -0400

    completed code for first release

--- server/config/db.js
+++ server/config/db.js
@@ -3,5 +3,7 @@
  * 
  */
 module.exports = { 
-    "URI": "mongodb://localhost/incidentrecords" 
+   // "URI": "mongodb://localhost/incidentrecords" 
+    //"URI": "mongodb://localhost/incidentrecords" 
+    "URI": "mongodb+srv://ash:lK5JCz1r7dbKy78a@mongodbserver.tiafv.mongodb.net/incidentrecords?retryWrites=true&w=majority"
 }
