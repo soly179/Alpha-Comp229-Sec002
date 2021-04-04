0086b57ce3026df0c5d8a3753283829c77761db5
soly179 <solyelsa@gmail.com>
2021-04-03 21:52:45 -0400

    first commit

---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let router = express.Router();
+
+let indexController = require('../controllers/index');
+
+/* GET home page. */
+router.get('/', indexController.displayHomePage);
+
+/* GET home page. */
+router.get('/home', indexController.displayHomePage);
+
+/* GET Contact Us page. */
+router.get('/contact', indexController.displayContactPage);
+
+module.exports = router;
